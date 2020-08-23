f9bab504aff9120e36b7fcd362c23d112cd21fa7
Wassim Chegham <github@wassim.dev>
2020-08-23 23:51:27 +0200

    style: reformate app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,24 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { NgxsModule } from '@ngxs/store';
-import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
-
-import { AppComponent } from './app.component';
-import { CameraComponent } from './camera.component';
-import { TimerComponent } from './timer.component';
-import { CameraRollComponent } from './camera-roll.component';
-import { environment } from 'src/environments/environment';
-import { TimerState as TimerState } from './store/timer.state';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { NgxsReduxDevtoolsPluginModule } from "@ngxs/devtools-plugin";
+import { NgxsModule } from "@ngxs/store";
+import { environment } from "src/environments/environment";
+import { AppComponent } from "./app.component";
+import { CameraRollComponent } from "./camera-roll.component";
+import { CameraComponent } from "./camera.component";
+import { TimerState } from "./store/timer.state";
+import { TimerComponent } from "./timer.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    CameraComponent,
-    TimerComponent,
-    CameraRollComponent
-  ],
+  declarations: [AppComponent, CameraComponent, TimerComponent, CameraRollComponent],
   imports: [
     BrowserModule,
     NgxsModule.forRoot([TimerState], {
-      developmentMode: !environment.production
+      developmentMode: !environment.production,
     }),
-    NgxsReduxDevtoolsPluginModule.forRoot()
+    NgxsReduxDevtoolsPluginModule.forRoot(),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
