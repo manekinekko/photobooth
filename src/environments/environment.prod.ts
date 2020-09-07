81dd70d3e3d6763288f1fc74576ab3b6e8cc32ef
Wassim Chegham <github@wassim.dev>
2020-09-07 23:39:55 +0200

    chore: setup msTeams SDK from npm

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+  production: true,
+  msTeams: {
+    websiteUrl: "https://delightful-field-0c06d3203.azurestaticapps.net",
+    contentUrl: "https://delightful-field-0c06d3203.azurestaticapps.net",
+    entityId: "dev.wassim.photobooth",
+    suggestedDisplayName: "Photo Booth",
+  },
 };
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { enableProdMode } from "@angular/core";
 import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
+import * as microsoftTeams from "@microsoft/teams-js";
 import "@tensorflow/tfjs-backend-cpu";
 import "@tensorflow/tfjs-backend-webgl";
 import { AppModule } from "./app/app.module";
@@ -11,4 +12,14 @@ if (environment.production) {
 
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
+  .then(() => {
+    if (environment.production) {
+      microsoftTeams.initialize();
+      microsoftTeams.settings.setValidityState(true);
+      microsoftTeams.settings.registerOnSaveHandler((saveEvent) => {
+        microsoftTeams.settings.setSettings(environment.msTeams);
+        saveEvent.notifySuccess();
+      });
+    }
+  })
   .catch((err) => console.error(err));
