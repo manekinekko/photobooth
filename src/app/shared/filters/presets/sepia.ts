a22a2bfa84aaa918d0f893b1f36b85b9db92c06b
Wassim Chegham <github@wassim.dev>
2020-09-07 12:15:29 +0200

    fix: allow passing cutom value for Sepia filter

--- src/app/shared/filters/presets/sepia.ts
+++ src/app/shared/filters/presets/sepia.ts
@@ -1,28 +1,47 @@
 import { colorMatrixShader } from "../shaders/color-matrix";
 
 export function sepia() {
-  return () => {
-    colorMatrixShader.call(this)([
-      0.393,
-      0.7689999,
-      0.18899999,
-      0,
-      0,
-      0.349,
-      0.6859999,
-      0.16799999,
-      0,
-      0,
-      0.272,
-      0.5339999,
-      0.13099999,
-      0,
-      0,
-      0,
-      0,
-      0,
-      1,
-      0,
-    ]);
+  /**
+   * value between 0 and 1
+   */
+  return (value: number) => {
+    const identity = [1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0];
+    const dot = (ma: number[], mb: number[], amount: number) =>
+      ma.map((a, i) => {
+        const b = mb[i];
+        return a + (b - a) * amount;
+      });
+
+    colorMatrixShader.call(this)(
+      dot(
+        identity,
+        [
+          0.393,
+          0.769,
+          0.189,
+          0,
+          0,
+          //
+          0.349,
+          0.686,
+          0.168,
+          0,
+          0,
+          //
+          0.272,
+          0.534,
+          0.131,
+          0,
+          0,
+          //
+          0,
+          0,
+          0,
+          1,
+          0,
+        ],
+        value
+      )
+    );
   };
 }
