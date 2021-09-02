5b305dd8adeac127ef013879154346abeecab580
Wassim Chegham <github@wassim.dev>
2021-09-02 22:20:58 +0200

    fix: type cast strength to number

--- src/app/app.worker.ts
+++ src/app/app.worker.ts
@@ -7,7 +7,7 @@ addEventListener('message', async ({ data }: { data: { image: ImageData, styleIm
   const image = new ImageData(data.image.data, data.image.width, data.image.height);
   const styleImg = new ImageData(data.styleImg.data, data.styleImg.width, data.styleImg.height);
   const model = new ArbitraryStyleTransferNetwork();
-  const styledImageData = await model.stylize(image, styleImg, data.strength);
+  const styledImageData = await model.stylize(image, styleImg, Number(data.strength));
 
   postMessage({
     styledImageData
